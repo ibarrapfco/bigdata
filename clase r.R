bb97ff8058eb7943bebbcd97954daf937ade030f
Francisco <francisco.ibarrap@utem.cl>
2020-05-30 00:14:14 -0400

    r update

--- clase r.R
+++ clase r.R
@@ -57,3 +57,58 @@ if(var3 > 5){
   var3 <- sqrt(var3)
   print(var3)
 }
+
+
+#######
+
+
+var5 <- -1
+
+
+if(var5 <= 0){
+  print("var5 es menor o igual a 0")
+  var5 <- 1
+  print(var5)
+} else {
+  print("var5 es mayor a 0")
+  var5 <- 2*var5**2
+  print(var5)
+}
+
+
+
+#####
+#si un numero es par, multipliquelo por 6
+#si es impar dividalo por 2 y retorne el resto
+
+
+x <- 29
+
+if(x %% 2 == 0){
+  print("si es par multiquicar por 6")
+  x <- 6 * x
+  print(x)
+} else {
+  print("si es impar dividir por 2")
+  x <- x/2
+  print(x)
+}
+
+
+####
+
+x <- 0
+
+if(x < 20){
+  # accion cuando x es menor a 5
+  x <- x ** 3
+  print(x)
+} else if (x == 0){
+  # accion cuando x es igual a 0
+  x <- x + 1
+  print(x)
+} else {
+  # accion cuando x es mayor a 0
+  x <- x / 2
+  print(x)
+}
